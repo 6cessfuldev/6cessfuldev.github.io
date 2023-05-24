b3ab1c792453d959c2cb004a4d79d48945f776b3
6cessfuldev <89137580+6cessfuldev@users.noreply.github.com>
2023-05-24 22:19:44 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	const rss = new RSS(
 	    document.querySelector("#rss-feeds"),
 	    //Change this to your own rss feeds
-        "https://feeds.feedburner.com/TechCrunch/startups",
+        "https://rss2json.com/#rss_url=https%3A%2F%2F6cessfuldev.tistory.com%2Frss",
 	    {
 		     // how many entries do you want?
 		    // default: 4
